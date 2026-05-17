3f85fbe9f880c374c139800959d269b3682ca39c
Sagar Kuvar <skuvar4@gmail.com>
2026-05-17 15:28:45 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ $(document).ready(function () {
     });
 
     var typed = new Typed(".typed", {
-        strings: ["Web Developer.", "App Developer.", "Student.","Data Scientist."],
+        strings: ["Web Developer.", "Cyber Security.", "Automation.","Data Scientist."],
         typeSpeed: 50,
         loop: true,
         startDelay: 400,
